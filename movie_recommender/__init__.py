c4ca3be798274b8cd6efd441afb001d55b16fd22
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-06 21:18:41 -0500

    Add: movie_recommender to package namespace

--- movie_recommender/__init__.py
+++ movie_recommender/__init__.py
@@ -1,4 +1,5 @@
-from .plot_similarity import PlotSimilarity
+from .movie_recommender import movie_recommender
 from .keyword_similarity import KeywordSimilarity
+from .plot_similarity import PlotSimilarity
 from .user_similarity import UserSimilarity
 from .utils import save, search
